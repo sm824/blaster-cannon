e6c11dc803ef51bcb4a88bea50592daded19e049
Sarah Mckenzie <sm0969565@gmail.com>
2024-05-16 20:06:25 -0600

    Imported original Cannon.js file

---
+++ game classes/Cannon.js
@@ -0,0 +1,89 @@
+class Cannon {
+    /**
+     * pos = the p5.Vector position of this cannon
+     * color1 = the color for the body of this cannon
+     * color2 = the color for the barrel of this cannon
+     */
+    constructor(pos, color1, color2) {
+      this.pos = pos;
+      this.colors = [color1, color2];
+  
+      // The attribute rotation uses degrees, and tracks the cannon's rotation
+      this.rotation = 0;
+  
+      this.bullets = [];
+    }
+  
+    /**
+     * Updates the cannon's rotation (in degrees)
+     * to point towards the mouse
+     */
+    aim() {
+      this.rotation =
+        Math.atan((mouseY - height / 2) / (mouseX - width / 2)) / (PI / 180) + 90;
+  
+      if (mouseX >= width / 2) {
+        this.rotation += 180;
+      }
+    }
+  
+    /**
+     * Creates a new bullet, fired from the cannon
+     */
+    fire() {
+      this.bullets.push(new Bullet(this.pos.copy(), this.rotation));
+    }
+  
+    /**
+     * Draws the cannon in colors specified by
+     * the items of the colors attribute
+     */
+    display() {
+      push();
+  
+      translate(this.pos.x, this.pos.y);
+      rotate(this.rotation);
+  
+      fill("grey");
+      circle(0, 0, 50); // Draws the circlular base
+  
+      fill(this.colors[1]);
+      rect(0, 25, 20, 35); // Draws the barrel
+  
+      fill(this.colors[0]);
+      square(0, 0, 42, 5); // Draws the body
+  
+      pop();
+    }
+  
+    /**
+     * Runs all the required functions and processes for
+     * the Cannon object to operate
+     */
+    operateCannon() {
+      this.aim();
+      this.display();
+  
+      // Monitors the bullets
+      for (let thisBullet = 0; thisBullet < this.bullets.length; thisBullet++) {
+        this.bullets[thisBullet].monitor();
+  
+        // Draws the bullets' bodies
+        push();
+  
+        fill("black");
+        circle(
+          this.bullets[thisBullet].bulletPosition.x,
+          this.bullets[thisBullet].bulletPosition.y,
+          this.bullets[thisBullet].diameter
+        );
+  
+        pop();
+  
+        if (this.bullets[thisBullet].isOffScreen) {
+          this.bullets.splice(thisBullet, 1);
+        }
+      }
+    }
+  }
+  
--- sketch.js
+++ sketch.js
@@ -1,9 +1,29 @@
+/*********************************************************************
+Name: Sarah Mckenzie
+Date: 2024-05-16
+Class: Computer Science 30
+Assignment: Final Project
+Title: Blaster Cannon - Bane of the Cybirds
+Purpose: Runs a game in which the user plays as a cannon able to leap
+         from walls, in which they must shoot down cyborg bird enemies
+*********************************************************************/
+DIMENSIONS = [800, 920];
+
+let canvas;
+
 function setup() {
-    createCanvas(400, 400);
+    canvas = createCanvas(...DIMENSIONS);
 
     console.log("Hello world!");
 }
 
 function draw() {
     background(220);
+
+    // Keeps the game canvas centered on the webpage
+    // Source: https://editor.p5js.org/jm8785/sketches/r0DMO5Mqj
+    canvas.position(
+        (windowWidth - DIMENSIONS[0]) / 2,
+        (windowHeight - DIMENSIONS[1]) / 2
+    );
 }
