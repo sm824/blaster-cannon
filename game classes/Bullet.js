b0b37938a9bd642dc9a22e69bed18fe2663b33ee
Sarah Mckenzie <sm0969565@gmail.com>
2024-05-20 18:53:47 -0600

    Fixed incorrect indents

--- game classes/Bullet.js
+++ game classes/Bullet.js
@@ -5,43 +5,42 @@ updated in a loop found in draw() of sketch.js
 *********************************************************************/
 
 class Bullet {
-    /**
-     * Sets initial attributes.
-     *
-     * startingBulletPosition = the starting bulletPositionition of the bullet,
-     *                          from which it will fly
-     * angle = the angle in degrees that the bullet will
-     *         move
-     */
-    constructor(startingBulletPosition, angle) {
-      this.pos = startingBulletPosition;
-      this.angle = angle;
-      this.diameter = 5;
-  
-      this.isOffScreen = false;
-  
-      // Moves the bullet to the tip of the cannon
-      advance(this.pos, 40, this.angle);
-    }
-  
-    /**
-     * Performs necessary procedures, such as monitoring
-     * for the bullet to go off the screen and moving it
-     * in its direction (determined by this.angle)
-     */
-    monitor() {
-      // Moves the bullet
-      advance(this.pos, SPEED, this.angle);
-  
-      // Determines if the bullet has gone off-screen
-      if (
-        this.pos.x < 0 ||
-        this.pos.x > width ||
-        this.pos.y < 0 ||
-        this.pos.y > height
-      ) {
-        this.isOffScreen = true;
-      }
+  /**
+   * Sets initial attributes.
+   *
+   * startingBulletPosition = the starting bulletPositionition of the bullet,
+   *                          from which it will fly
+   * angle = the angle in degrees that the bullet will
+   *         move
+   */
+  constructor(startingBulletPosition, angle) {
+    this.pos = startingBulletPosition;
+    this.angle = angle;
+    this.diameter = 5;
+
+    this.isOffScreen = false;
+
+    // Moves the bullet to the tip of the cannon
+    advance(this.pos, 40, this.angle);
+  }
+
+  /**
+   * Performs necessary procedures, such as monitoring
+   * for the bullet to go off the screen and moving it
+   * in its direction (determined by this.angle)
+   */
+  monitor() {
+    // Moves the bullet
+    advance(this.pos, SPEED, this.angle);
+
+    // Determines if the bullet has gone off-screen
+    if (
+      this.pos.x < 0 ||
+      this.pos.x > width ||
+      this.pos.y < 0 ||
+      this.pos.y > height
+    ) {
+      this.isOffScreen = true;
     }
   }
-  
+}
