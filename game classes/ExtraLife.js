a27df398d1163921da7a1e84c691083d8c566f3d
Sarah Mckenzie <sm0969565@gmail.com>
2024-06-07 17:09:17 -0600

    Added glowing graphics around extra lives

--- game classes/ExtraLife.js
+++ game classes/ExtraLife.js
@@ -1,14 +1,14 @@
 
 class ExtraLife {
 
-    static heartColor = "#cc3340";
+    static heartColor = [204, 51, 64];
 
     /**
      * Prepares the heart by setting necessary attributes
      */
     constructor(pos) {
         this.pos = pos;
-        this.currentFrame = random(0, 60);
+        this.currentFrame = random(0, 30);
         this.isOffScreen = false;
     }
 
@@ -18,10 +18,19 @@ class ExtraLife {
     display() {
         push();
         translate(0, -cameraY);
-        fill(ExtraLife.heartColor);
 
-        // Fills in the gap down the center
+        // Draws the glowing area around the heart
+        fill(...ExtraLife.heartColor, 100 - this.currentFrame * 3);
+        ellipseMode(CENTER);
+        noStroke();
+
+        circle(this.pos.x, this.pos.y, 80);
+
+        // Fills in the gap down the center of the heart
+        fill(ExtraLife.heartColor);
         stroke(ExtraLife.heartColor);
+        strokeWeight(1);
+        
         line(
             this.pos.x, this.pos.y - 15 * EXTRA_LIFE_SIZE,
             this.pos.x, this.pos.y + 30 * EXTRA_LIFE_SIZE
@@ -48,6 +57,14 @@ class ExtraLife {
      * inside the Cannon class
      */
     monitor() {
+
+        // Manages the frames
+        this.currentFrame++;
+
+        if (this.currentFrame >= 30) {
+            this.currentFrame = 0;
+        }
+
         this.display();
 
         advance(this.pos, SPEED / 2, 0);
